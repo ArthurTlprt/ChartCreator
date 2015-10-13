39be35fb30987dc7530200688f7d7718eac8ce18
ArthurT <arthur.talpaert@isen-lille.fr>
2015-10-13 09:47:49 +0200

    1 cercle rouge

--- main.cpp
+++ main.cpp
@@ -13,22 +13,22 @@
 
 int main(int argc, char** argv) {
   
-    MyWindow* win = MyWindow::createWindow(0, 0, 800, 600);
+    MyWindow* win = MyWindow::createWindow(0, 0, 1400, 800);
 
     Disc disc0;
-    disc0.setRadius(20);
+    disc0.setRadius(200);
     
     //usleep(1000000);   // 1 seconds = 1000000 microseconds
     
-    for (int i = 1; i < 20; i++) 
+    for (int i = 1; i < 2; i++) 
     {
       disc0.setColor(i*12, 0, 0);
-      disc0.setCenter(Point3D(40*i, 300, 0));
+      disc0.setCenter(Point3D(400*i, 300, 0));
       win->draw(disc0);
       usleep(50000);  
     }
     
-    usleep(5000000);  
+    usleep(1000000);  
     
     MyWindow::destroyWindow();
     return 0;
