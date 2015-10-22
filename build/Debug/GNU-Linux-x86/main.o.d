24fd58d5f441b3c4243850591c11cbbd92607f72
ArthurT <arthur.talpaert@isen-lille.fr>
2015-10-22 18:37:02 +0200

    add * to checkout

---
+++ build/Debug/GNU-Linux-x86/main.o.d
@@ -0,0 +1,22 @@
+build/Debug/GNU-Linux-x86/main.o: main.cpp MyWindow.h MyDrawable.h \
+ Color.h Disc.h Point3D.h Shape.h Rectangle.h BarGraph1.h Chart.h Data1.h
+
+MyWindow.h:
+
+MyDrawable.h:
+
+Color.h:
+
+Disc.h:
+
+Point3D.h:
+
+Shape.h:
+
+Rectangle.h:
+
+BarGraph1.h:
+
+Chart.h:
+
+Data1.h:
